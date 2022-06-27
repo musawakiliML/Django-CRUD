49312e3722dca3a5a2bc17d7636aea68e0d222f8
musawakiliML <musawakiliml@hotmail.com>
2022-06-27 10:47:31 +0100

    fixed url routing main page

--- I4G000112NET/urls.py
+++ I4G000112NET/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import PostListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls", namespace="blog"))
+    path("blog/", include("blog.urls", namespace="blog")),
+    path("", PostListView.as_view(), name="all")
 ]
